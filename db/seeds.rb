1292fdb4a1ce430a522aef25ca0d0b61abdce4dc
Catherine Rice <clr2107@gmail.com>
2017-10-16 01:04:11 -0400

    add more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,8 +4,8 @@ Card.delete_all
 Deck.delete_all
 User.delete_all
 
-User.create(username: "cat", email: "cat@cat.com", password: "cat1")
-User.create(username: "pan", email: "pan@pan.com", password: "pan1")
+User.create(username: "Cat", email: "cat@cat.com", password: "cat1")
+User.create(username: "Amber", email: "amber@amber.com", password: "amber1")
 
 Deck.create(name: "Computer Science")
 Card.create(question: "What does HTML stand for?", answer: "Hypertext markup language", deck_id: 1)
@@ -16,4 +16,9 @@ Deck.create(name: "Music")
 Card.create(question: "How many symphonies did Beethoven write?", answer: "9", deck_id: 2)
 Card.create(question: "Which Romantic composer died in an asylum?", answer: "Robert Schumann", deck_id: 2)
 Card.create(question: "What is a lied: a song, a symphony, or a concerto?", answer: "song", deck_id: 2)
+
+Deck.create(name: "Math")
+Card.create(question: "2^2", answer: "4", deck_id: 3)
+Card.create(question: "6*8", answer: "48", deck_id: 3)
+
 
